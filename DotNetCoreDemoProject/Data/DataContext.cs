5ebdb5c3dbdaf7ff729db06a47cd1ba5b0d3daa7
shagun-abjima <153979823+shagun-abjima@users.noreply.github.com>
2024-03-12 17:20:19 +0530

    initial commit

---
+++ DotNetCoreDemoProject/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DotNetCoreDemoProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DotNetCoreDemoProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+
+
+}
+
+
+
